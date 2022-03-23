aa61d1d07a7b957e4c38c6e3c754f0796bea123a
Alistair Houghton <alistair.g.houghton@gmail.com>
2022-03-23 16:12:30 +0000

    finished models and added time formatter

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,20 +6,34 @@ const userController = {
   getAllUsers(req, res) {
     User.find({})
       .select('-__v')
-      .then(dbUserData => res.json(dbUserData))
+      .then(userData => res.json(userData))
       .catch(err => {
         console.log(err);
         res.status(400).json(err);
       });
   },
+  // get single user by id
 
-  // -------------------------------------------------
+  getOneUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .populate('friends')
+      .populate('thoughts')
+      .then((userData) => {
+        if (!userData) {
+          return res.status(404).json({ message: 'No user with this id!' });
+        }
+        res.json(userData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 
-    // get single user by id
-    
-  };
+};
 
-  module.exports = userController;
+module.exports = userController;
 
 
 
