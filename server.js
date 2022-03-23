6f5827936a553fdc1d6ebc76d03c17ffc10bbc2e
Alistair Houghton <alistair.g.houghton@gmail.com>
2022-03-23 16:29:39 +0000

    added in packages and updated structure for routes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -11,8 +11,7 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server for ${activity} running on port ${PORT}!`);
+    console.log(`API server running on port ${PORT}!`);
   });
 });
-
 
